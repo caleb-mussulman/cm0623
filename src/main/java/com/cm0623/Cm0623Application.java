fe0c1eb1c93b901e55498ee846e06740551de542
caleb.mussulman <caleb.mussulman@gmail.com>
2023-06-03 14:45:15 -0500

    initial commit for project setup

---
+++ src/main/java/com/cm0623/Cm0623Application.java
@@ -0,0 +1,13 @@
+package com.cm0623;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.cm0623")
+public class Cm0623Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cm0623Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cm0623/Cm0623ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cm0623;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cm0623ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
