b563b71dea7bcbda22a58a72a520fd097a349d54
caleb.mussulman <caleb.mussulman@gmail.com>
2023-06-03 17:30:21 -0500

    Added H2 DB model and data

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+CREATE SCHEMA IF NOT EXISTS depot;
+SET SCHEMA depot;
+
+CREATE TABLE tool_type(
+    id BIGINT PRIMARY KEY,
+    tool_name VARCHAR NOT NULL,
+    charge_amount DECIMAL(15,2) NOT NULL,
+    is_weekday_charge BOOLEAN NOT NULL,
+    is_weekend_charge BOOLEAN NOT NULL,
+    is_holiday_charge BOOLEAN NOT NULL
+);
+
+CREATE TABLE tool(
+    code VARCHAR PRIMARY KEY,
+    tool_type_id BIGINT,
+    brand VARCHAR
+);
+
+ALTER TABLE tool
+ADD FOREIGN KEY (tool_type_id)
+REFERENCES tool_type(id);
